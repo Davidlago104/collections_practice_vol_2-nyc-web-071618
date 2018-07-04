774a26ff010ec5655c3e6e849266ed159abefc0f
Davidlago104 <davidlago104@gmail.com>
2018-07-04 19:22:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,10 @@
 # your code goes here
+def begins_with_r(array)
+  wordswithr = []
+  array.each do |word|
+    if word.start_with?("R")
+  wordswithr << true 
+else
+  wordswithr << false 
+end
+end
